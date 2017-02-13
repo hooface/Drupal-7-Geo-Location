e88938447549adc9423950c254b47bb73397a90c
www.itech-planet.net <contact@itech-planet.net>
2017-02-13 23:50:17 +0100

    URL update
    Server url change

--- app/src/main/java/net/iplanet/drupal/v7/Tokens.java
+++ app/src/main/java/net/iplanet/drupal/v7/Tokens.java
@@ -7,8 +7,8 @@ public class Tokens {
     public static final String KEY_SESSION_NAME_RESPONSE = "session_name";
     public static final String KEY_SESSION_ID_RESPONSE = "sessid";
     public static final String KEY_TOKEN_RESPONSE = "token";
-    public static final String serv_end_Pnt_DOMAIN = ".sc-d7-02.itech-planet.net";
-    public static final String serv_end_Pnt_LOGIN = "http://sc-d7-02.itech-planet.net/rest/user/login";
-    public static final String serv_end_Pnt_LOGOUT = "http://sc-d7-02.itech-planet.net/rest/user/logout";
-    public static final String serv_end_Pnt_NODE = "http://sc-d7-02.itech-planet.net/rest/node";
+    public static final String serv_end_Pnt_DOMAIN = ".<server-url>";
+    public static final String serv_end_Pnt_LOGIN = "http://<server-url>/rest/user/login";
+    public static final String serv_end_Pnt_LOGOUT = "http://<server-url>/rest/user/logout";
+    public static final String serv_end_Pnt_NODE = "http://<server-url>/rest/node";
 }
